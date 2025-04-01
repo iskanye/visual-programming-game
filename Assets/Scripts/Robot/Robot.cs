c53a2d4c147bc7b59f91ec2f80b160dc2bb1c85d
Iskander Biktashev <iskanderbikta@yandex.ru>
2025-04-02 01:26:09 +0300

    robot linearly moving w anim

--- Assets/Scripts/Robot/Robot.cs
+++ Assets/Scripts/Robot/Robot.cs
@@ -1,9 +1,31 @@
 using UnityEngine;
+using System.Collections;
 
 public class Robot : MonoBehaviour
 {
+    public bool IsMoving { get => moving; }
+
+    [SerializeField] private float speed;
+
+    private bool moving;
+
     public void Move(Vector3Int dir)
     {
-        transform.position += (Vector3)dir;
+        var startPos = transform.position;
+        float t = 0;
+        
+        IEnumerator _Move()
+        {
+            moving = true;
+            while (transform.position != startPos + dir) 
+            {
+                transform.position = Vector3.Lerp(startPos, startPos + dir, t);
+                t += speed * Time.deltaTime;
+                yield return null;
+            }
+            moving = false;
+        }
+
+        StartCoroutine(_Move());
     }
 }
--- Assets/Scripts/Visual Programming/Blocks/MoveBlock.cs
+++ Assets/Scripts/Visual Programming/Blocks/MoveBlock.cs
@@ -8,6 +8,11 @@ public class MoveBlock : Block
     public override IEnumerator Process(object[] data)
     {
         Blueprint.Current.Robot.Move(direction);
+        while (Blueprint.Current.Robot.IsMoving) 
+        {
+            yield return null;
+        }
+        
         yield return base.Process(null);
         output[0].Output(null);
     }
